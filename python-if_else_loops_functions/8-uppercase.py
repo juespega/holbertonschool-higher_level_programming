71a0803afb2e4a06d9412a92a48c57efee6bda0a
juespega <juespega666@gmail.com>
2023-06-01 12:02:58 -0500

    8. To uppercase

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def uppercase(s):
+    for char in s:  # recorrer el string caracter por caracter
+        ascii_val = ord(char)  # asignar el valor en num entero del caracter
+        # validar si el num está en el rango de min(ASCII range: 97-122)
+        if 97 <= ascii_val <= 122:
+            # conversión a may, se resta 32 al valor actual, lo que da la letra en may
+            uppercase_char = chr(ascii_val - 32)
+            print(uppercase_char, end='')
+        else:
+            print(char, end='')
+    print()  # imprimir nueva línea
