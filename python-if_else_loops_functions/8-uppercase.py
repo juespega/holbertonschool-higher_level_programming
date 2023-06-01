328f2b83fe23f44512ec1bb76f052023a605a3ac
juespega <juespega666@gmail.com>
2023-06-01 12:14:24 -0500

    8. To uppercase

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-def uppercase(s):
-    for char in s:
+def uppercase(str):
+    for char in str:
         ascii_val = ord(char)
         if 97 <= ascii_val <= 122:
             uppercase_char = chr(ascii_val - 32)
