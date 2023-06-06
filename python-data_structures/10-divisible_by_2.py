97aaae2445c7396cd79fb3432156c1fe317dde92
juespega <juespega666@gmail.com>
2023-06-06 11:10:40 -0500

    10. Only by 2

---
+++ python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    if not my_list:
+        return None
+    else:
+        return [num % 2 == 0 for num in my_list]
+        # list_result = []
+        # for item in my_list:
+        #     if item % 2 == 0:
+        #         list_result[item] = True
+        #     else:
+        #         list_result[item] = False
+        # return list_result
