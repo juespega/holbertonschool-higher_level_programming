f37269721f8fb24245fb74ce1ee400d8427c26f9
juespega <juespega666@gmail.com>
2023-06-06 09:39:07 -0500

    7. Tuples addition

---
+++ python-data_structures/7-add_tuple.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    # Completar con ceros si la tupla es menor a 2 elementos
+    if len(tuple_a) < 2:
+        tuple_a += (0,) * (2 - len(tuple_a))
+    if len(tuple_b) < 2:
+        tuple_b += (0,) * (2 - len(tuple_b))
+
+    # Sumar solo los primeros 2 elementos si la tupla es mayor a 2 elementos
+    tuple_a = tuple_a[:2]
+    tuple_b = tuple_b[:2]
+    
+    # Sumar los elementos correspondientes
+    suma_tuplas = tuple(x + y for x, y in zip(tuple_a, tuple_b))
+    
+    return suma_tuplas
