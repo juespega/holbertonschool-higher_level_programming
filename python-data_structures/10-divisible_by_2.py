aaedbd64b661f7b9bd5da58fbd679f0db81029a3
juespega <juespega666@gmail.com>
2023-06-06 11:15:12 -0500

    10. Only by 2

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -3,11 +3,11 @@ def divisible_by_2(my_list=[]):
     if not my_list:
         return None
     else:
-        return [num % 2 == 0 for num in my_list]
-        # list_result = []
-        # for item in my_list:
-        #     if item % 2 == 0:
-        #         list_result[item] = True
-        #     else:
-        #         list_result[item] = False
-        # return list_result
+        # return [num % 2 == 0 for num in my_list]
+        list_result = my_list[:]
+        for item in my_list:
+            if item % 2 == 0:
+                list_result[item] = True
+            else:
+                list_result[item] = False
+        return list_result
