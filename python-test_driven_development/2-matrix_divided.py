73af3654df05d2e7d03fb3562018f184df142604
juespega <juespega666@gmail.com>
2023-06-16 22:12:16 -0500

    1. Divide a matrix

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -16,20 +16,19 @@ def matrix_divided(matrix, div):
         raise TypeError(
             "matrix must be a matrix (list of lists) of integers/floats")
 
-    # Check if each row of the matrix has the same size
-    row_size = len(matrix[0])
-    if not all(len(row) == row_size for row in matrix):
+    """Check if each row of the matrix has the same size"""
+    if len(set(len(row) for row in matrix)) > 1:
         raise TypeError("Each row of the matrix must have the same size")
 
-    # Check if div is a number
+    """Check if div is a number"""
     if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
 
-    # Check if div is not zero
+    """Check if div is not zero"""
     if div == 0:
         raise ZeroDivisionError("division by zero")
 
     # Divide each element of the matrix by div and round to 2 decimal places
-    new_matrix = [[round(num / div, 2) for num in row] for row in matrix]
+    # new_matrix = [[round(num / div, 2) for num in row] for row in matrix]
 
-    return new_matrix
+    return ([list(map(lambda x: round(x / div, 2), row)) for row in matrix])
