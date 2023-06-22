6dab9321116c7b31361dd3a5dc6eecb4a34f6488
juespega <juespega666@gmail.com>
2023-06-22 14:32:33 -0500

    0. Read file

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""
+function that reads a text file (UTF8) and prints it to stdout:
+"""
+
 
 def read_file(filename=""):
     """
@@ -19,4 +23,3 @@ def read_file(filename=""):
     with open(filename, 'r', encoding='utf-8') as file:
         for line in file:
             print(line, end='')
-
