6c03699bfde0acf7cd92370dbaca1cae1a00afcc
juespega <juespega666@gmail.com>
2023-06-22 15:01:01 -0500

    2. Append to a file

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+
+def append_write(filename="", text=""):
+    """
+    Appends a string to the end of a text file (UTF-8) and
+    returns the number of characters added.
+
+    Arguments:
+    - filename (str): The name of the file to append to (optional).
+    If not provided, an empty string is assumed.
+    - text (str): The string to append to the file (optional).
+    If not provided, an empty string is assumed.
+
+    If the file doesn't exist, it will be created.
+
+    Usage:
+    - append_write('file.txt', 'Hello, world!')  # Appends the string
+    to the file
+
+    Returns:
+    - int: The number of characters added to the file.
+
+    """
+
+    with open(filename, 'a', encoding='utf-8') as file:
+        num_characters = file.write(text)
+        return num_characters
