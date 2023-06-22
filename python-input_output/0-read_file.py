194208527b53fcc663a89bd778f0efcdfbfe0da7
juespega <juespega666@gmail.com>
2023-06-22 14:29:38 -0500

    0. Read file

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+def read_file(filename=""):
+    """
+    Reads a UTF-8 encoded text file and prints it to stdout.
+
+    Arguments:
+    - filename (str): The name of the file to read (optional). If not provided,
+      an empty string is assumed.
+
+    No file permission or file existence exceptions are handled, as the 'with'
+    statement is used.
+
+    Usage:
+    - read_file()                 # Reads a file without specifying the name
+    - read_file('file.txt')       # Reads the 'file.txt' file
+
+    """
+    with open(filename, 'r', encoding='utf-8') as file:
+        for line in file:
+            print(line, end='')
+
