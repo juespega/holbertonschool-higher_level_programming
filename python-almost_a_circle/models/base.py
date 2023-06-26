62167ef0db553132f002b830ecbab6e99be48081
juespega <juespega666@gmail.com>
2023-06-26 15:50:10 -0500

    1. Base class

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -15,7 +15,11 @@ class Base:
         Class constructor
         """
         if id is not None:
+            """If an "id" value is provided, assign it to the "id"
+            instance variable"""
             self.id = id
         else:
+            # Increment the object counter for the class
             Base.__nb_objects += 1
+            # Assign the current counter value to the "id" instance variable
             self.id = Base.__nb_objects
