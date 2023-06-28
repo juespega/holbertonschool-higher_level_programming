7f01ca94bc7e941b9cf4bb4f2e940b06c836d415
juespega <juespega666@gmail.com>
2023-06-28 11:49:03 -0500

    10. And now, the Square!

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -1,36 +1,33 @@
 #!/usr/bin/python3
-"""
-Crate a Square class inheriting from the Rectangle class
-"""
-# Import the Rectangle class from the "rectangle" module
+"""class """
+
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
     """
-    This class represents a square and inherits from the
-    Rectangle class.
+    This class represents a Square and inherits from the Rectangle class.
     """
 
     def __init__(self, size, x=0, y=0, id=None):
         """
-        Initializes a Square object.
-
-        Args:
-            size (int): The size of the square.
-            x (int, optional): The x-coordinate of the square's position.
-            Defaults to 0.
-            y (int, optional): The y-coordinate of the square's position.
-            Defaults to 0.
-            id (int, optional): The ID of the square. Defaults to None.
+        Initializes an instance of the Square class.
         """
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
         """
-        Returns a string representation of the Square instance.
-
-        Returns:
-            str: The string representation of the Square instance.
+        Returns a string representation of the Square.
         """
         return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
+
+    @property
+    def size(self):
+        """ Getter size """
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """ Setter size """
+        self.width = value
+        self.height = value
