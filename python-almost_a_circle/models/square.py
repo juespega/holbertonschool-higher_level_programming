34d812b0a00162f70033ffaaa318822abe46886b
juespega <juespega666@gmail.com>
2023-06-28 11:57:56 -0500

    10. And now, the Square!

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -1,33 +1,61 @@
 #!/usr/bin/python3
-"""class """
-
+"""
+Crate a Square class inheriting from the Rectangle class
+"""
+# Import the Rectangle class from the "rectangle" module
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
     """
-    This class represents a Square and inherits from the Rectangle class.
+    This class represents a square and inherits from the
+    Rectangle class.
     """
 
     def __init__(self, size, x=0, y=0, id=None):
         """
-        Initializes an instance of the Square class.
+        Initializes a Square object.
+
+        Args:
+            size (int): The size of the square.
+            x (int, optional): The x-coordinate of the square's position.
+            Defaults to 0.
+            y (int, optional): The y-coordinate of the square's position.
+            Defaults to 0.
+            id (int, optional): The ID of the square. Defaults to None.
         """
         super().__init__(size, size, x, y, id)
 
-    def __str__(self):
-        """
-        Returns a string representation of the Square.
-        """
-        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
-
     @property
     def size(self):
-        """ Getter size """
+        """
+        Getter for the size attribute.
+
+        Returns:
+            int: The size of the square.
+        """
         return self.width
 
     @size.setter
     def size(self, value):
-        """ Setter size """
+        """
+        Setter for the size attribute.
+
+        Args:
+            value (int): The size value to set.
+
+        Raises:
+            ValueError: If the size value is not greater than 0.
+            TypeError: If the size value is not an integer.
+        """
         self.width = value
         self.height = value
+
+    def __str__(self):
+        """
+        Returns a string representation of the Square instance.
+
+        Returns:
+            str: The string representation of the Square instance.
+        """
+        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
