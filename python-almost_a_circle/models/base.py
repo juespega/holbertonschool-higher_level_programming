4b7a697048dd51e3717d0d8c6f663a148d331fc6
juespega <juespega666@gmail.com>
2023-06-30 13:26:10 -0500

    base

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,42 +1,96 @@
 #!/usr/bin/python3
-"""
-Crate a base class.
-"""
-import json
+"""Define the Base Class"""
+import turtle
 
 
 class Base:
-    """
-    This class will be the “base” of all other classes in this project.
-    """
+    """constructor"""
     __nb_objects = 0
 
+    """Function Initialized"""
+
     def __init__(self, id=None):
-        """
-        Class constructor
-        """
         if id is not None:
-            """If an "id" value is provided, assign it to the "id"
-            instance variable"""
             self.id = id
         else:
-            # Increment the object counter for the class
             Base.__nb_objects += 1
-            # Assign the current counter value to the "id" instance variable
             self.id = Base.__nb_objects
 
     @staticmethod
     def to_json_string(list_dictionaries):
-        """
-        Returns the JSON string representation of a list of dictionaries.
-
-        Args:
-            list_dictionaries (list): The list of dictionaries to
-            convert to JSON.
-
-        Returns:
-            str: The JSON string representation of the list of dictionaries.
-        """
+        import json
         if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
         return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        filename = cls.__name__ + '.json'
+        with open(filename, 'w') as json_file:
+            if list_objs is None:
+                json_file.write("[]")
+            else:
+                list_dict = [o.to_dictionary() for o in list_objs]
+                json_file.write(Base.to_json_string(list_dict))
+
+    @staticmethod
+    def from_json_string(json_string):
+        import json
+        if json_string is None or len(json_string) == 0:
+            return []
+        return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        if cls.__name__ == 'Rectangle':
+            dummy = cls(1, 1)
+        elif cls.__name__ == 'Square':
+            dummy = cls(1)
+        else:
+            dummy = None
+        dummy.update(**dictionary)
+        return dummy
+
+    @classmethod
+    def load_from_file(cls):
+        filename = str(cls.__name__) + ".json"
+        try:
+            with open(filename, "r") as jsonfile:
+                list_dicts = Base.from_json_string(jsonfile.read())
+                return [cls.create(**d) for d in list_dicts]
+        except IOError:
+            return []
+
+    @staticmethod
+    def draw(list_rectangles, list_squares):
+        # Create a Turtle screen
+        screen = turtle.Screen()
+
+        # Create a Turtle object
+        t = turtle.Turtle()
+
+        # Set the speed of the turtle
+        t.speed(2)
+
+        # Draw rectangles
+        for rectangle in list_rectangles:
+            t.penup()
+            t.goto(rectangle.x, rectangle.y)
+            t.pendown()
+            for _ in range(2):
+                t.forward(rectangle.width)
+                t.left(90)
+                t.forward(rectangle.height)
+                t.left(90)
+
+        # Draw squares
+        for square in list_squares:
+            t.penup()
+            t.goto(square.x, square.y)
+            t.pendown()
+            for _ in range(4):
+                t.forward(square.size)
+                t.left(90)
+
+        # Close the Turtle graphics window
+        turtle.done()
